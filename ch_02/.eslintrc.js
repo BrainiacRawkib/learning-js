4fcbcd201bc32d6c91b3cae1c407b7e451c832cc
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-02-17 03:32:12 -0800

    modified .eslintrc.js

--- ch_02/.eslintrc.js
+++ ch_02/.eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        // "es2021": true
     },
-    "extends": [
-        "standard"
-    ],
+    "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "rules": {
+        /* changed comma-dangle default...ironically,
+        we can't use a dangling comma here because this
+        is a JSON file*/
+        "comma-dangle": [
+            2, "always-multiline"
+        ],
+        "indent": [
+            2, 4
+        ],
     }
 }
